6d43e1d2d88d64a1ad6ef1527527069db59c6f55
sugita177 <moriaki.sugita.1730@gmail.com>
2023-11-15 17:12:35 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+
+//const mousePointX = ref(0);
+//const mousePointY = ref(0);
+const particlePoints = ref([]);
+
+const createParticle = e => {
+  console.log(e.clientX, e.clientY);
+  particlePoints.value.push([e.clientX, e.clientY]);
+  console.log(particlePoints.value);
+}
+
+const drawParticle = (Point) => {
+  ;
+}
+
+</script>
+
+<template>
+  <main>
+    <canvas width="600" height="400" @click="createParticle">
+    </canvas>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
