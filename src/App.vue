7799d540486ee90c77503b0937f45b4378204cad
sugita177 <moriaki.sugita.1730@gmail.com>
2023-11-17 10:51:59 +0900

    粒子移動機能実装途中

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,11 @@ import { ref } from 'vue';
 //const mousePointX = ref(0);
 //const mousePointY = ref(0);
 
-class particle {
+class Particle {
   point_;
   radius_;
   color_;
+  velocity_;
 
   constructor(point, radius, color) {
     this.point_  = point;
@@ -15,38 +16,86 @@ class particle {
     this.color_  = color;
   }
 
-  drawParticle() {
+  drawSelfParticle(context, offset) {
+    //console.log(this.point_);
+    context.beginPath();
+    context.fillStyle = this.color_;
+    context.arc(-offset[0] + this.point_[0], -offset[1] + this.point_[1], this.radius_, 0, 2 * Math.PI);
+    context.fill();
+  }
 
+  removeSelfParticleDrawing(context, offset) {
+    context.beginPath();
+    context.fillStyle = 'red';
+    context.arc(-offset[0] + this.point_[0], -offset[1] + this.point_[1], this.radius_, 0, 2 * Math.PI);
+    context.fill();
   }
 }
 
-
-
-const particlePoints = ref([]);
+const particles = ref([]);
+const isParticleSelected = ref(false);
+const selectedParticleNumber =ref(-1);
 
 const createParticle = e => {
   //console.log(e.clientX, e.clientY);
-  const point = [e.clientX, e.clientY];
-  particlePoints.value.push(point);
+  if(isParticleSelected.value === false) {
+    const particlePoint = [e.clientX, e.clientY];
+  const particle = new Particle(particlePoint, 20, 'green');
+  particles.value.push(particle);
   const context = e.target.getContext('2d');
-  const canvasLeftTop = [
+  const canvasOffSet = [
     e.target.getBoundingClientRect().left,
     e.target.getBoundingClientRect().top
   ]
-  drawParticle(context, point, 20, canvasLeftTop);
+  particle.drawSelfParticle(context, canvasOffSet);
+  }
 }
 
-const drawParticle = (context, point, radius, origin) => {
-  context.beginPath();
-  context.arc(-origin[0] + point[0], -origin[1] + point[1], radius, 0, 2 * Math.PI);
-  context.fill();
+const isExistPatricle = e => {
+  for(let i=0; i<particles.value.length; i++) {
+    const distanceFromPrticle = Math.sqrt((e.clientX - particles.value[i].point_[0])**2 + (e.clientY - particles.value[i].point_[1])**2);
+    //console.log(`particle${i}:${distanceFromPrticle}`);
+    if(distanceFromPrticle <= particles.value[i].radius_) {
+      isParticleSelected.value = true;
+      selectedParticleNumber.value = i;
+      const context = e.target.getContext('2d');
+      const canvasOffSet = [
+        e.target.getBoundingClientRect().left,
+        e.target.getBoundingClientRect().top
+      ]
+      particles.value[i].removeSelfParticleDrawing(context,canvasOffSet)
+      //console.log(e.clientX, e.clientY);
+      //console.log(`particle${i}`);
+    }
+  }
+}
+
+const drawMovingParticle = e => {
+  if(isParticleSelected.value) {
+    ;
+  }
+}
+
+const moveParticle = e => {
+  if(isParticleSelected.value) {
+    const particle = particles.value[selectedParticleNumber.value];
+    particle.point_ = [e.clientX, e.clientY];
+    const context = e.target.getContext('2d');
+    const canvasOffSet = [
+    e.target.getBoundingClientRect().left,
+    e.target.getBoundingClientRect().top
+    ]
+    particle.drawSelfParticle(context, canvasOffSet);
+    isParticleSelected.value = false;
+    selectedParticleNumber.value = -1;
+  }
 }
 
 </script>
 
 <template>
   <main>
-    <canvas width="600" height="400" @click="createParticle">
+    <canvas width="1000" height="1000" @click="createParticle" @mousedown="isExistPatricle" @mousemove="drawMovingParticle" @mouseup="moveParticle">
     </canvas>
   </main>
 </template>
